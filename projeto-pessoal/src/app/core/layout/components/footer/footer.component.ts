16bccfc6c31648be80edd5dcb1c73bdfc9325232
fco-romario <fco.romario.alves@gmail.com>
2025-12-24 19:34:23 -0300

    feat: Adiciona footer

---
+++ projeto-pessoal/src/app/core/layout/components/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'estudo-footer',
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FooterComponent {
+
+}
--- projeto-pessoal/src/app/core/layout/layout.component.ts
+++ projeto-pessoal/src/app/core/layout/layout.component.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { RouterOutlet } from "@angular/router";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'estudo-layout',
-  imports: [HeaderComponent, SidenavComponent, RouterOutlet],
+  imports: [HeaderComponent, SidenavComponent, RouterOutlet, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
