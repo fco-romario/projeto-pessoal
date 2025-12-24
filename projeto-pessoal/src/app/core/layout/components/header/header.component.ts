7e66b635d87af18d43f2edbfea5bbff0a1d34e01
fco-romario <fco.romario.alves@gmail.com>
2025-12-24 16:17:31 -0300

    feat: adiciona Header ao projeto

---
+++ projeto-pessoal/src/app/core/layout/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'estudo-header',
+  imports: [MatToolbarModule, MatIconModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HeaderComponent {
+
+}
