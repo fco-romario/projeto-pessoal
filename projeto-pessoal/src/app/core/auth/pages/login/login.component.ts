86e9c5c1ba8c14930f23d383a34d7d998abd0960
fco-romario <fco.romario.alves@gmail.com>
2025-12-25 18:14:14 -0300

    feat: Cria estilo da tela de login com sections

--- projeto-pessoal/src/app/core/auth/pages/login/login.component.ts
+++ projeto-pessoal/src/app/core/auth/pages/login/login.component.ts
@@ -1,12 +1,30 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: 'estudo-login',
-  imports: [],
+  imports: [ReactiveFormsModule, MatInputModule, MatButtonModule, MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent {
+  hide = signal(true);
 
+  form = new FormGroup({
+    user: new FormControl('', {validators: [Validators.required]}),
+    password: new FormControl('', {validators: [Validators.required]}),
+  });
+
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+  submit() {
+    console.log('form', this.form.value);
+  }
 }
