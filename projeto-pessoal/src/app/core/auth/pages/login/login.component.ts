1de7f7041f3a087c4e3b6f22d9c40aa750e67875
fco-romario <fco.romario.alves@gmail.com>
2025-12-27 18:03:08 -0300

    feat: Cria auth service e método de login básico

--- projeto-pessoal/src/app/core/auth/pages/login/login.component.ts
+++ projeto-pessoal/src/app/core/auth/pages/login/login.component.ts
@@ -5,6 +5,10 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from "@angular/material/icon";
 import { ActivatedRoute, Router, RouterLink } from "@angular/router";
 import { AuthFormButtonsComponent } from "../components/auth-form-buttons/auth-form-buttons.component";
+import { AuthService } from '../../services/auth.service';
+import { AuthTokenResponse } from '../../interfaces/auth-token-response';
+import { UserCredentials } from '../../interfaces/user-credentials';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'estudo-login',
@@ -16,6 +20,7 @@ import { AuthFormButtonsComponent } from "../components/auth-form-buttons/auth-f
 export class LoginComponent {
   private readonly _activatedRoute = inject(ActivatedRoute);
   private readonly _router = inject(Router);
+  private readonly _authService = inject(AuthService);
 
   hide = signal(true);
 
@@ -30,7 +35,17 @@ export class LoginComponent {
   }
 
   submit() {
-    console.log('form', this.form.value);
+    this._authService.login({...this.form.value} as UserCredentials)
+    .subscribe({
+      next: (token: AuthTokenResponse) => {
+        this._router.navigate(['../home'], { relativeTo: this._activatedRoute });
+      },
+      error: (error: HttpErrorResponse) => {
+        if(error.status === 401) {
+          this.form.setErrors({invalidCredentials: true});
+        }
+      }
+    });
   }
  
   criarConta() {
---
+++ projeto-pessoal/src/app/core/auth/services/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { UserCredentials } from '../interfaces/user-credentials';
+import { Observable, of, throwError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AuthTokenResponse } from '../interfaces/auth-token-response';
+
+function generateToken(): string {
+  let token = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 20; i++) {
+    token += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return token;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  
+  login(payload: UserCredentials): Observable<AuthTokenResponse> {
+    if(payload.user === 'romario@gmail.com' && payload.password === '123456') {
+      return of({token: generateToken()});
+    }
+
+    return throwError(
+      () => new HttpErrorResponse({
+        status: 401,
+        statusText: 'Unauthorized'  
+      })
+    );
+  }
+}
