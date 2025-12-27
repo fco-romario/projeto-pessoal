ca2ac58c03896b16b9627c59abb4e52360811675
fco-romario <fco.romario.alves@gmail.com>
2025-12-26 22:37:38 -0300

    feat: Adiciona validator personalisado para verificar se senhas combinam

--- projeto-pessoal/src/app/core/auth/pages/signup/signup.component.ts
+++ projeto-pessoal/src/app/core/auth/pages/signup/signup.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { MatFormField, MatInputModule, MatLabel } from "@angular/material/input";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from '@angular/material/button';
+import { passwordMatchValidator } from '../../validators/password-match.validators';
 
 @Component({
   selector: 'estudo-signup',
@@ -16,11 +17,27 @@ export class SignupComponent {
   hide = signal(true);
 
   form = new FormGroup({
-    name: new FormControl('', {validators: [Validators.required, ]}),
-    email: new FormControl('', {validators: [Validators.required, Validators.email]}),
-    password: new FormControl('', {validators: [Validators.required]}),
-    passwordConfirm: new FormControl('', {validators: [Validators.required]}),
-  });
+    name: new FormControl('', {validators: [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(50)
+    ]}),
+    email: new FormControl('', {validators: [
+      Validators.required,
+      Validators.email,
+      Validators.maxLength(50)]}),
+    password: new FormControl('', {validators: [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(8),
+    ]}),
+    confirmPassword: new FormControl('', {validators: [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(8),
+    ]}),
+  }, {validators: passwordMatchValidator}
+);
 
   clickEvent(event: MouseEvent) {
     this.hide.set(!this.hide());
