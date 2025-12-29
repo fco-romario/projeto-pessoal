db8ccbed6e9f6c8f5b08391cc240f4bbc8467cd4
fco-romario <fco.romario.alves@gmail.com>
2025-12-28 21:25:59 -0300

    refactor: melhorando código duplicado em login facade

--- projeto-pessoal/src/app/core/auth/facades/login-facade.service.ts
+++ projeto-pessoal/src/app/core/auth/facades/login-facade.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { UserCredentials } from '../interfaces/user-credentials';
-import { tap, switchMap, Observable } from 'rxjs';
+import { tap, switchMap, Observable, pipe } from 'rxjs';
 import { AuthTokenResponse } from '../interfaces/auth-token-response';
 import { User } from '../interfaces/user';
 import { LoggedInUserStoreService } from '../stores/logged-in-user-store.service';
@@ -16,20 +16,18 @@ export class LoginFacadeService {
   private readonly _loggedInUserStoreService = inject(LoggedInUserStoreService);
   
   login(payload: UserCredentials): Observable<User> {
-    return this._authService.login(payload)
-      .pipe(
-        tap((response: AuthTokenResponse) => this._tokenLocalStorageStore.set(response.token)),
-        switchMap((response: AuthTokenResponse) => this._authService.getCurrentUser(response.token)),
-        tap((user: User) => this._loggedInUserStoreService.setUser(user)),
-      );
+    return this._authService.login(payload).pipe(this.creatUserSession());
   }
 
   refreshToken(token: string): Observable<User> {
-    return this._authService.refreshToken(token)
-      .pipe(
-        tap((response: AuthTokenResponse) => this._tokenLocalStorageStore.set(response.token)),
-        switchMap((response: AuthTokenResponse) => this._authService.getCurrentUser(response.token)),
-        tap((user: User) => this._loggedInUserStoreService.setUser(user)),
-      );
+    return this._authService.refreshToken(token).pipe(this.creatUserSession());
+  }
+
+  private creatUserSession() {
+    return pipe(
+      tap((response: AuthTokenResponse) => this._tokenLocalStorageStore.set(response.token)),
+      switchMap((response: AuthTokenResponse) => this._authService.getCurrentUser(response.token)),
+      tap((user: User) => this._loggedInUserStoreService.setUser(user)),
+    );
   }
 }
