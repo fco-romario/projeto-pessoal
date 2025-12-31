8a74bd82b0e49511cd265b0b3b3d9c27a4cd26af
fco-romario <fco.romario.alves@gmail.com>
2025-12-30 23:52:27 -0300

    feat: Melhora content do router outlet

--- projeto-pessoal/src/app/features/user/user.component.ts
+++ projeto-pessoal/src/app/features/user/user.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'estudo-user',
-  imports: [],
+  imports: [MatToolbarModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
