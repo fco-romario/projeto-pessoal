fd8a9acb8528c6e8b63704b3840e710d8d6d0c5c
fco-romario <fco.romario.alves@gmail.com>
2025-12-31 14:52:23 -0300

    feat: Adiciona campos de dados pessoais

--- projeto-pessoal/src/app/features/user/user.component.ts
+++ projeto-pessoal/src/app/features/user/user.component.ts
@@ -1,13 +1,34 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'estudo-user',
-  imports: [MatToolbarModule],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserComponent {
 
+  readonly form = new FormGroup({
+    name: new FormControl('', {validators:
+      [ 
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(150),
+      ]}),
+    mathersName : new FormControl('', {validators: [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(150),
+     ]}),
+    gener: new FormControl('', {validators: [ Validators.required ]}),
+    phoneNumber: new FormControl('', {validators: [ Validators.required ]}),// adicionar validação de telefone
+    cpf: new FormControl('', {validators: [ Validators.required ]}), // adicionar validação de cpf
+    rg: new FormControl('', {validators: [ Validators.required ]}), // adicionar validação de rg
+  });
+
 }
+
