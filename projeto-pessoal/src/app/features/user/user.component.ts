20219505286fc97a4ccb6bb36f308d4ffe2f1c86
fco-romario <fco.romario.alves@gmail.com>
2025-12-31 17:28:43 -0300

    feat: Adiciona enderecos ao módulo de usuário

--- projeto-pessoal/src/app/features/user/user.component.ts
+++ projeto-pessoal/src/app/features/user/user.component.ts
@@ -1,18 +1,36 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'estudo-user',
-  imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatButtonModule, MatIconModule],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatButtonModule, MatIconModule, MatExpansionModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserComponent {
+  readonly panelOpenState = signal(false);
+
+  private fb = inject(FormBuilder);
+
+  enderecosGet= computed(() => {
+    return this.form.get('enderecos') as FormArray;
+  })
+
+  enderecos = new FormGroup({
+    cep: new FormControl('', {validators: [ Validators.required ]}), // adicionar validação de cep
+    logradouro: new FormControl('', {validators: [ Validators.required ]}),
+    bairro: new FormControl('', {validators: [ Validators.required ]}),
+    // cidade: new FormControl('', {validators: [ Validators.required ]}),
+    // estado: new FormControl('', {validators: [ Validators.required ]}),
+    numero: new FormControl('', {validators: [ Validators.required ]}),
+    complemento: new FormControl(''),
+  })
 
   readonly form = new FormGroup({
     name: new FormControl('', {validators:
@@ -30,6 +48,7 @@ export class UserComponent {
     phoneNumber: new FormControl('', {validators: [ Validators.required ]}),// adicionar validação de telefone
     cpf: new FormControl('', {validators: [ Validators.required ]}), // adicionar validação de cpf
     rg: new FormControl('', {validators: [ Validators.required ]}), // adicionar validação de rg
+    enderecos: this.fb.array([this.enderecos, this.enderecos], Validators.required),
   });
 
 }
