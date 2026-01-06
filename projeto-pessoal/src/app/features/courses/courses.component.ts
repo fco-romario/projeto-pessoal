ac93347f4d0e7b18b79781c02aa5121b3ae76500
fco-romario <fco.romario.alves@gmail.com>
2026-01-05 22:48:15 -0300

    feat: Adiciona Table para cursos

--- projeto-pessoal/src/app/features/courses/courses.component.ts
+++ projeto-pessoal/src/app/features/courses/courses.component.ts
@@ -1,12 +1,36 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+
+export interface UserData {
+  id: string;
+  name: string;
+  url: string;
+  date: Date;
+}
 
 @Component({
   selector: 'estudo-courses',
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CoursesComponent {
+  displayedColumns: string[] = ['id', 'name', 'url', 'date'];
+  dataSource: MatTableDataSource<UserData>;
 
+  constructor() {
+    this.dataSource = new MatTableDataSource(
+      [
+        { 
+          id: '1', name: 'Angular + Spring', url: 'https://www.youtube.com/watch?v=tPOMG0D57S0&list=PLGxZ4Rq3BOBoSRcKWEdQACbUCNWLczg2G', date: new Date() 
+        },
+        { 
+          id: '2', name: 'Angular + Spring', url: 'https://www.youtube.com/watch?v=tPOMG0D57S0&list=PLGxZ4Rq3BOBoSRcKWEdQACbUCNWLczg2G', date: new Date() 
+        },
+        { 
+          id: '3', name: 'Angular + Spring', url: 'https://www.youtube.com/watch?v=tPOMG0D57S0&list=PLGxZ4Rq3BOBoSRcKWEdQACbUCNWLczg2G', date: new Date() 
+        },
+      ]);
+   }
 }
