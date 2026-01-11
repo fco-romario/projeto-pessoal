10b0992825d1b566cdb276f8bdc20fef4c78871b
fco-romario <fco.romario.alves@gmail.com>
2026-01-11 16:16:59 -0300

    feat: Adiciona barra de progresso para passar um feedback do estado da requisição

--- projeto-pessoal/src/app/core/layout/layout.component.ts
+++ projeto-pessoal/src/app/core/layout/layout.component.ts
@@ -5,20 +5,25 @@ import { RouterOutlet } from "@angular/router";
 import { FooterComponent } from "./components/footer/footer.component";
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BreadcrumbStore } from './stores/breadcrumb-store';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { LoadingStore } from './stores/loading-store';
 
 @Component({
   selector: 'estudo-layout',
-  imports: [HeaderComponent, SidenavComponent, RouterOutlet, FooterComponent, MatToolbarModule],
+  imports: [HeaderComponent, SidenavComponent, RouterOutlet, FooterComponent, MatToolbarModule, MatProgressBarModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LayoutComponent {
   private readonly _breadcrumbStore = inject(BreadcrumbStore);
+  private readonly _loadingStore = inject(LoadingStore);
 
   public tree = computed(() => {
     return this._breadcrumbStore.breadcrumbs().join(' • ');
   });
+
+  public isLoading = computed(() => this._loadingStore.isLoading());
 
   public lastOnetree = computed(() => {
     return this._breadcrumbStore.breadcrumbs()[this._breadcrumbStore.breadcrumbs().length - 1];
