702d71f188c473e19b3110a16b5583a7e2f96dd9
fco-romario <fco.romario.alves@gmail.com>
2026-01-24 13:39:35 -0300

    feat: Adiciona a arvore de rota link navegaveis

--- projeto-pessoal/src/app/core/layout/layout.component.ts
+++ projeto-pessoal/src/app/core/layout/layout.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
-import { RouterOutlet } from "@angular/router";
+import { RouterOutlet, RouterLinkWithHref, Router, ActivatedRoute } from "@angular/router";
 import { FooterComponent } from "./components/footer/footer.component";
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BreadcrumbStore } from './stores/breadcrumb-store';
@@ -10,7 +10,7 @@ import { LoadingStore } from './stores/loading-store';
 
 @Component({
   selector: 'estudo-layout',
-  imports: [HeaderComponent, SidenavComponent, RouterOutlet, FooterComponent, MatToolbarModule, MatProgressBarModule],
+  imports: [HeaderComponent, SidenavComponent, RouterOutlet, FooterComponent, MatToolbarModule, MatProgressBarModule, RouterLinkWithHref],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -18,9 +18,12 @@ import { LoadingStore } from './stores/loading-store';
 export class LayoutComponent {
   private readonly _breadcrumbStore = inject(BreadcrumbStore);
   private readonly _loadingStore = inject(LoadingStore);
+  private readonly _router = inject(Router);
+  private readonly _activatedRoute = inject(ActivatedRoute);
 
   public tree = computed(() => {
-    return this._breadcrumbStore.breadcrumbs().join(' • ');
+    return this._breadcrumbStore.breadcrumbs();
+    // return this._breadcrumbStore.breadcrumbs().join(' • ');
   });
 
   public isLoading = computed(() => this._loadingStore.isLoading());
@@ -28,4 +31,11 @@ export class LayoutComponent {
   public lastOnetree = computed(() => {
     return this._breadcrumbStore.breadcrumbs()[this._breadcrumbStore.breadcrumbs().length - 1];
   });
+
+  redirect(tree: string) {
+    switch(tree) {
+      case 'Cursos' : return 'courses';
+      default: return '';
+    }
+  }
 }
